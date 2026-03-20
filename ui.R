59e3d2f7e1cfa8b92f709e8622c65914a595df2e
amsparks <37578485+amsparks@users.noreply.github.com>
2026-03-20 13:11:33 +0000

    Move user guide to top

--- ui.R
+++ ui.R
@@ -90,8 +90,8 @@ ui <- function(input, output, session) {
       widths = c(2, 8),
       well = FALSE,
       # Content for these panels is defined in the R/ui_panels/ folder
-      twm_tab_panel(),
       user_guide_panel(),
+      twm_tab_panel(),
       shiny::tabPanel(
         value = "a11y_panel",
         "Accessibility",
