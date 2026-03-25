55a36fff171b1ff64a1413d227f231e7fe049d64
amsparks <37578485+amsparks@users.noreply.github.com>
2026-03-25 14:51:44 +0000

    Add/amend initial UI and unit tests

---
+++ tests/testthat/test-shinytest2.R
@@ -0,0 +1,123 @@
+library(shinytest2)
+
+
+all_inputs <- c(
+  "tabsetpanels",
+
+  # Teacher demand trajectories
+  "file_type_pupil_teacher",
+  "filter_phase",
+  "main_tabs_pupil_teacher",
+
+  # PGITT trainee need time series
+  "file_type_pgitt_need",
+  "filter_phase_pgitt_need",
+  "filter_subject_pgitt_need",
+  "main_tabs_pgitt_trainee_need",
+
+  # Drivers of PGITT trainee need changes
+  "file_type_drivers",
+  "filter_phase_drivers",
+  "filter_subject_drivers",
+  "main_tabs_drivers_analysis",
+
+  # Flow trajectories
+  "file_type_flows",
+  "filter_phase_flow",
+  "filter_subject_flow",
+  "filter_flow_type",
+  "main_tabs_flow_trajectories"
+)
+
+
+all_outputs <- c(
+  # Teacher demand trajectories tab
+  "pupil_teacher_plot",
+  "tablePupilTeacher",
+  "download_button_ui_pupil_teacher",
+  "pt_summary_box",
+
+  # PGITT trainee need time series tab
+  "pgitt_need_timeseries_plot",
+  "tablePgittNeedTimeseries",
+  "download_button_ui_pgitt_need",
+
+  # Drivers tab
+  "drivers_waterfall_plot",
+  "table_pgitt_need_diff",
+  "table_drivers_breakdown",
+  "download_button_ui_drivers",
+
+  # Flow trajectories tab
+  "flow_timeseries_plot",
+  "table_flow_trajectories",
+  "download_button_ui_flows"
+)
+
+
+test_that("{shinytest2} recording: initial_state", {
+  app <- AppDriver$new(name = "initial_state")
+  app$click("cookies_banner-cookies_reject")
+  app$set_inputs(navlistPanel = "Teacher demand and PGITT need")
+  app$set_inputs(tabsetpanels = "Teacher demand trajectories")
+  app$set_inputs(filter_phase = "Secondary")
+  app$set_inputs(main_tabs_pupil_teacher = "Table")
+  app$set_inputs(filter_phase = "Primary")
+  app$set_inputs(main_tabs_pupil_teacher = "Chart")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_pupil_teacher = "Download")
+  app$set_inputs(file_type_pupil_teacher = "XLSX (Up to X.XX MB)")
+  app$set_inputs(file_type_pupil_teacher = "JPEG (Up to XXX KB)")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(tabsetpanels = "PGITT trainee need calculation")
+  app$set_inputs(tabsetpanels = "PGITT trainee need time series")
+  app$set_inputs(filter_phase_pgitt_need = "Secondary")
+  app$set_inputs(filter_subject_pgitt_need = "Computing")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_pgitt_trainee_need = "Table")
+  app$set_inputs(filter_phase_pgitt_need = "Primary")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_pgitt_trainee_need = "Download")
+  app$set_inputs(file_type_pgitt_need = "XLSX (Up to X.XX MB)")
+  app$set_inputs(file_type_pgitt_need = "JPEG (Up to XXX KB)")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(tabsetpanels = "Drivers of PGITT trainee need changes")
+  app$set_inputs(filter_phase_drivers = "Secondary")
+  app$set_inputs(filter_subject_drivers = "Physical Education")
+  app$set_inputs(main_tabs_drivers_analysis = "Table")
+  app$set_inputs(filter_subject_drivers = "Religious Education")
+  app$set_inputs(main_tabs_drivers_analysis = "Download")
+  app$set_inputs(file_type_drivers = "XLSX (Up to X.XX MB)")
+  app$set_inputs(file_type_drivers = "JPEG (Up to XXX KB)")
+  app$set_inputs(main_tabs_drivers_analysis = "Chart")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_drivers_analysis = "Table")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_drivers_analysis = "Download")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(tabsetpanels = "Flow trajectories")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(filter_phase_flow = "Secondary")
+  app$set_inputs(filter_subject_flow = "Modern Foreign Languages")
+  app$set_inputs(filter_flow_type = "Returners")
+  app$set_inputs(filter_subject_flow = "Physics")
+  app$set_inputs(filter_flow_type = "Total leaver rate")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_flow_trajectories = "Table")
+  app$set_inputs(filter_phase_flow = "Primary")
+  app$set_inputs(filter_flow_type = "Returners")
+  app$set_inputs(main_tabs_flow_trajectories = "Download")
+  app$set_inputs(file_type_flows = "XLSX (Up to X.XX MB)")
+  app$set_inputs(file_type_flows = "JPEG (Up to XXX KB)")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_flow_trajectories = "Table")
+  app$set_inputs(filter_phase_flow = "Secondary")
+  app$set_inputs(filter_subject_flow = "Classics")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(main_tabs_flow_trajectories = "Chart")
+  app$set_inputs(filter_subject_flow = "Physics")
+  app$expect_values(input = all_inputs, output = all_outputs)
+  app$set_inputs(navlistPanel = "a11y_panel")
+  app$set_inputs(navlistPanel = "cookies_panel_ui")
+  app$set_inputs(navlistPanel = "support_panel_ui")
+})
