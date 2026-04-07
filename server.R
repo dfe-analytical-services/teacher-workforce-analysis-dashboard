3aa88218eb9b43549a6799246892ffde59b80700
amsparks <37578485+amsparks@users.noreply.github.com>
2026-04-07 21:20:21 +0100

    Move pupil teacher summary helpers functions from server to helper functions script

--- server.R
+++ server.R
@@ -320,50 +320,34 @@ server <- function(input, output, session) {
     }
   )
 
-  # Create dataframe of pupil and teacher number changes between 24/25 and 27/28
+  ## Section for pupil/teacher number blue summary box
+  ## See helper functions script for calc_pt_change_24_to_27() + build_pupil_teacher_summary() functions
+
+  # Reactive: Calculate pupil and teacher changes between 2024/25 and 2027/28
 
   pt_change_24_to_27 <- reactive({
+    # Ensure filtered pupil/teacher data is available before proceeding
     req(pt_data_filtered())
 
-    pt_data_filtered() %>%
-      filter(start_year %in% c(2024, 2027)) %>%
-      arrange(start_year) %>%
-      mutate(
-        pupil_diff   = pupil_numbers - lag(pupil_numbers),
-        pupil_pct    = (pupil_diff / lag(pupil_numbers)) * 100,
-        teacher_diff = teacher_numbers - lag(teacher_numbers),
-        teacher_pct  = (teacher_diff / lag(teacher_numbers)) * 100
-      )
+    # Apply helper function to calculate absolute and percentage changes
+    # between 2023/24 and 2027/28
+    calc_pt_change_24_to_27(pt_data_filtered())
   })
 
-  # Create summary dataframe
+  # Reactive: Build summary text describing projected changes
 
   pupil_teacher_summary <- reactive({
-    df <- pt_change_24_to_27()
-    df_27 <- df[df$start_year == 2027, ]
-
-    # Determine direction words
-    pupil_dir <- if (df_27$pupil_diff > 0) "more" else "fewer"
-    teacher_dir <- if (df_27$teacher_diff > 0) "higher" else "lower"
-
-    pupil_diff_txt <- scales::label_comma()(abs(df_27$pupil_diff))
-    teacher_diff_txt <- scales::label_comma()(abs(df_27$teacher_diff))
-
-    pupil_pct_txt <- scales::label_number(accuracy = 0.1, suffix = "%")(df_27$pupil_pct)
-    teacher_pct_txt <- scales::label_number(accuracy = 0.1, suffix = "%")(df_27$teacher_pct)
-
-    glue::glue(
-      "We project {pupil_diff_txt} {pupil_dir} pupils ({pupil_pct_txt}) ",
-      "and teacher demand to be {teacher_diff_txt} {teacher_dir} ({teacher_pct_txt}) ",
-      "in 2027/28 compared to 2024/25. (dummy)"
-    )
+    # Generate a human-readable summary sentence based on the calculated changes
+    build_pupil_teacher_summary(pt_change_24_to_27())
   })
 
-  # Pupil teacher summary box
+  # Output: Render pupil and teacher summary text in the UI
 
   output$pt_summary_box <- renderText({
+    # Display the summary sentence in the summary box
     pupil_teacher_summary()
   })
+
 
 
   # # PGITT trainee need time series tab ----------------------------------------------------------------------------
