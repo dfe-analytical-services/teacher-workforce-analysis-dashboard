7235da35067fa461ac3ea0373bd6e51efd6780ed
amsparks <37578485+amsparks@users.noreply.github.com>
2026-04-22 21:33:44 +0100

    Update tests to work with publication datasets

--- tests/testthat/test_read_data_functions.R
+++ tests/testthat/test_read_data_functions.R
@@ -13,7 +13,7 @@
 
 write_test_parquet <- function(df) {
   tmp <- tempfile(fileext = ".parquet")
-  arrow::write_parquet(df, tmp)
+  write_parquet(df, tmp)
   tmp
 }
 
@@ -32,14 +32,14 @@ expect_missing_cols_error <- function(expr) {
 # Does the function return a dataframe with the correct number of rows,
 # 'start year' is created and values are numeric and rounded?
 
-test_that("read_pupil_teacher_numbers() cleans, rounds, and creates start_year", {
+test_that("read_pupil_teacher_numbers() cleans names, rounds .5s up, and derives start_year", {
   # Dummy input parquet
   input <- tibble::tibble(
-    academic_year = c("2020/21", "2021/22"),
-    pupil_numbers = c(1234.8, 5678.2),
-    teacher_numbers = c(400.4, 999.6),
-    projection = c("Yes", "Yes"),
-    phase = "Primary"
+    `Academic year` = c("2020/21", "2021/22"),
+    `Pupil numbers` = c(1234.8, 5678.2),
+    `Teacher numbers` = c(400.5, 999.6),
+    Projection = c("Yes", "Yes"),
+    Phase = "Primary"
   )
 
   file <- write_test_parquet(input)
@@ -47,17 +47,30 @@ test_that("read_pupil_teacher_numbers() cleans, rounds, and creates start_year",
   # Run function
   out <- read_pupil_teacher_numbers(file)
 
-  # ---- Structure checks ----
+  # Structure checks
   expect_s3_class(out, "data.frame") # check it returns a tibble/data frame
   expect_equal(nrow(out), 2) # check the number of rows are identical to input
 
-  # ---- Column creation ----
-  expect_true("start_year" %in% names(out)) # check it creates expected columns
+  # Column name cleaning checks
+  expect_true(
+    all(
+      c(
+        "academic_year",
+        "pupil_numbers",
+        "teacher_numbers",
+        "projection",
+        "phase"
+      ) %in% names(out)
+    )
+  )
 
-  # Check computed values are numeric and rounded
-  expect_equal(out$start_year, c(2020, 2021))
+  # Start year column check
+  expect_true("start_year" %in% names(out)) # check it creates expected columns
+  expect_equal(out$start_year, c(2020L, 2021L)) # check correct
+
+  # Rounding check
   expect_equal(out$pupil_numbers, c(1235, 5678))
-  expect_equal(out$teacher_numbers, c(400, 1000))
+  expect_equal(out$teacher_numbers, c(401, 1000))
 })
 
 # Does the function throw an error when required columns are missing?
@@ -72,8 +85,7 @@ test_that("read_pupil_teacher_numbers throws error when required columns missing
   )
 
   # Write temp parquet file
-  tmp <- tempfile(fileext = ".parquet")
-  arrow::write_parquet(bad_df, tmp)
+  tmp <- write_test_parquet(bad_df)
 
   expect_missing_cols_error(
     read_pupil_teacher_numbers(tmp)
@@ -81,18 +93,19 @@ test_that("read_pupil_teacher_numbers throws error when required columns missing
 })
 
 
-# 2. Tests for read_pgitt_need_timeseries() -----------------------------------------------------------------------
+# 2 - Tests for read_pgitt_need_timeseries() -----------------------------------------------------------------------
 
 # Does the function return a dataframe, have the column names expected, extract start year,
 # call subject = total for primary otherwise keep original subject values?
 
-test_that("read_pgitt_need_timeseries() renames and derives subject correctly", {
+test_that("read_pgitt_need_timeseries() cleans names, renames phase and derives start_year", {
   input <- tibble::tibble(
-    time_period = c("2019/20", "2020/21", "2020/21"),
-    subject_filter_group = c("Primary", "Secondary", "Secondary"),
-    subject = c("English", "Maths", "Biology"),
-    pgitt_trainee_need_count = c("6", "6", "6"),
-    difference_to_previous_year_percent = c("6", "6", "6")
+    `Time period` = c("2019/20", "2020/21", "2020/21"),
+    `Education phase` = c("Primary", "Secondary", "Secondary"),
+    Subject = c("English", "Maths", "Biology"),
+    `PGITT trainee need count` = c("6", "6", "6"),
+    `Difference to previous year count` = c("6", "6", "6"),
+    `Difference to previous year percent` = c("6", "6", "6")
   )
 
   file <- write_test_parquet(input)
@@ -101,43 +114,68 @@ test_that("read_pgitt_need_timeseries() renames and derives subject correctly", 
 
   # Structure
   expect_s3_class(out, "data.frame") # check it returns a dataframe
-  expect_true(all(c("phase", "subject") %in% names(out))) # check certain columns are present
+  expect_equal(nrow(out), 3) # check the data has the correct number of rows
 
-  # Start year extracted from first 4 chars
-  expect_equal(out$start_year, c(2019, 2020, 2020))
 
-  # Primary → subject forced to "Total"
-  expect_equal(out$subject[1], "Total")
+  # Column cleaning & renaming
+  expect_true(
+    all(
+      c(
+        "subject",
+        "phase",
+        "pgitt_trainee_need_count",
+        "difference_to_previous_year_count",
+        "difference_to_previous_year_percent"
+      ) %in% names(out)
+    )
+  )
 
-  # Secondary → keep original
-  expect_equal(out$subject[2], "Maths")
-  expect_equal(out$subject[3], "Biology")
+
+  # Created column checks
+  expect_true("start_year" %in% names(out))
+  expect_true("academic_year" %in% names(out))
+
+  # start_year derived from first 4 characters
+  expect_equal(out$start_year, c(2019L, 2020L, 2020L))
+
+
+  # academic_year formatting checks
+  expect_equal(
+    out$academic_year,
+    c("2019/20", "2020/21", "2020/21")
+  )
+
+  # Subject values preserved
+  expect_equal(out$subject, c("English", "Maths", "Biology"))
+
+  # Phase correctly renamed
+  expect_equal(out$phase, c("Primary", "Secondary", "Secondary"))
 })
+
 
 # Does the function throw an error when required columns are missing?
 
 test_that("read_pgitt_need_timeseries throws error when required columns missing", {
   bad_df <- tibble::tibble(
+    # education_phase and difference_to_previous_year_count missing
     time_period = "2020/21",
     subject = "English",
     pgitt_trainee_need_count = 200,
     difference_to_previous_year_percent = 0.1
-    # education_phase missing
   )
 
-  tmp <- tempfile(fileext = ".parquet")
-  arrow::write_parquet(bad_df, tmp)
+  tmp <- write_test_parquet(bad_df)
 
   expect_missing_cols_error(
     read_pgitt_need_timeseries(tmp)
   )
 })
 
-# 3. Tests for read_drivers_data() --------------------------------------------------------------------------------
+# 3 - Tests for read_drivers_data() --------------------------------------------------------------------------------
 
 # Does the function have clean column names and round values to 1dp?
 
-test_that("read_drivers_data() cleans names and returns all rows intact", {
+test_that("read_drivers_data() cleans names, preserves rows and rounds values to 1 decimal place", {
   input <- tibble::tibble(
     "Driver" = c("Entrants", "Leavers"),
     "Value" = c(100.123, 200.876),
@@ -147,11 +185,29 @@ test_that("read_drivers_data() cleans names and returns all rows intact", {
 
   file <- write_test_parquet(input)
 
+  # Run function
   out <- read_drivers_data(file)
 
+  # Structure checks
   expect_s3_class(out, "data.frame") # check it returns a df
-  expect_true(all(c("driver", "value", "phase", "subject") %in% names(out))) # check columns are there
-  expect_equal(out$value, c(100.1, 200.9)) # check values are rounded to 1dp
+  expect_equal(nrow(out), 2)
+
+  # Column name cleaning
+
+  expect_true(
+    all(
+      c("driver", "value", "phase", "subject") %in% names(out)
+    )
+  )
+
+  # Rounding checks
+  expect_equal(out$value, c(100.1, 200.9))
+  expect_true(is.numeric(out$value))
+
+  # Other columns preserved
+  expect_equal(out$driver, c("Entrants", "Leavers"))
+  expect_equal(out$phase, c("Secondary", "Secondary"))
+  expect_equal(out$subject, c("Biology", "Biology"))
 })
 
 # Does the function throw an error when a column is missing?
@@ -172,59 +228,146 @@ test_that("read_drivers_data throws error when required columns missing", {
   )
 })
 
-# =============================================================================
-# 4) Tests for read_flows_data()
-# =============================================================================
 
-# Does the function return a df and get units, years and version correctly?
+# 4 - Tests for read_flows_*_publication_data() -------------------------------------------------------------------
 
-test_that("read_flows_data() derives units, year, and version correctly", {
-  input <- tibble::tibble(
-    Year = c("2024/25", "2025/26"),
-    Type = c("Total leaver rate", "Newly qualified entrants"),
-    Phase = c("Secondary", "Secondary"),
-    Subject = c("Biology", "Biology"),
-    Value = c(0.006, 6),
-    Historic_or_trajectory = c("Trajectory", "Trajectory"),
-    Publication_year = c("2026", "2026")
-  )
+## read_flows_2025_publication_data checks
 
-  file <- write_test_parquet(input)
+# Does the function have clean column names, derives start_year and drops rows with NA for value?
 
-  out <- read_flows_data(file)
+test_that(
+  "read_flows_2025_publication_data cleans names, derives start_year, and drops NA values",
+  {
+    input <- tibble(
+      Phase = c("Secondary", "Secondary", "Secondary"),
+      Subject = c("Biology", "Biology", "Biology"),
+      Type = c("NQE trajectory", "NQE trajectory", "NQE trajectory"),
+      Academic_year = c("2024/25", "2025/26", "2026/27"),
+      Value = c(1.2, 1.3, NA), # third row should be dropped
+      Unit = c("FTE", "FTE", "FTE"),
+      Historic_or_trajectory = "Trajectory",
+      Publication_year = 2025
+    )
 
-  # Structure
-  expect_s3_class(out, "data.frame")
+    file <- write_test_parquet(input)
 
-  # Unit assignment logic
-  expect_equal(out$unit, c("%", "FTE"))
+    out <- read_flows_2025_publication_data(file)
 
-  # Extract numeric year
-  expect_equal(out$start_year, c(2024, 2025))
+    # Structure checks
+    expect_s3_class(out, "data.frame")
+    expect_equal(nrow(out), 2) # NA row removed
 
-  # Version column injected
-  expect_true("version" %in% names(out))
-  expect_equal(out$version, c("Last year", "Last year"))
-})
+    # Required columns checks
+    expect_true(
+      all(
+        c(
+          "phase",
+          "subject",
+          "type",
+          "academic_year",
+          "value",
+          "unit",
+          "historic_or_trajectory",
+          "publication_year"
+        ) %in% names(out)
+      )
+    )
+
+    # Derived column check
+    expect_true("start_year" %in% names(out))
+    expect_equal(out$start_year, c(2024L, 2025L))
+  }
+)
 
 # Does the function throw an error when a column is missing?
 
-test_that("read_flows_data throws error when required columns missing", {
-  bad_df <- tibble::tibble(
-    Phase = "Secondary",
-    Subject = "Physics",
-    Type = "Total leaver rate",
-    Year = "2020/21",
-    Value = 0.1,
-    Unit = "%",
-    Publication_year = 2025
-    # historic_or_trajectory missing
-  )
+test_that(
+  "read_flows_2025_publication_data throws an error when required columns are missing",
+  {
+    bad_df <- tibble(
+      phase = "Secondary",
+      subject = "Physics",
+      value = 1.5,
+      academic_year = "2024/25",
+      unit = "FTE",
+      publication_year = 2025
+      # missing type and historic_or_trajectory
+    )
 
-  tmp <- tempfile(fileext = ".parquet")
-  arrow::write_parquet(bad_df, tmp)
+    tmp <- write_test_parquet(bad_df)
 
-  expect_missing_cols_error(
-    read_flows_data(tmp)
-  )
-})
+    expect_missing_cols_error(
+      read_flows_2025_publication_data(tmp)
+    )
+  }
+)
+
+## read_flows_2026_publication_data checks
+
+# Does the function have clean column names, derives start_year and drops rows with NA for value?
+
+test_that(
+  "read_flows_2026_publication_data cleans names, derives start_year, and drops NA values",
+  {
+    input <- tibble(
+      Phase = c("Secondary", "Secondary", "Secondary"),
+      Subject = c("Maths", "Maths", "Maths"),
+      Type = c("NQE trajectory", "NQE trajectory", "NQE trajectory"),
+      Academic_year = c("2025/26", "2026/27", "2027/28"),
+      Value = c(2.1, 2.2, NA), # third row should be dropped
+      Unit = c("FTE", "FTE", "FTE"),
+      Historic_or_trajectory = "Trajectory",
+      Publication_year = 2026
+    )
+
+    file <- write_test_parquet(input)
+
+    out <- read_flows_2026_publication_data(file)
+
+    # Structure checks
+    expect_s3_class(out, "data.frame")
+    expect_equal(nrow(out), 2)
+
+    # Required columns checks
+    expect_true(
+      all(
+        c(
+          "phase",
+          "subject",
+          "type",
+          "academic_year",
+          "value",
+          "unit",
+          "historic_or_trajectory",
+          "publication_year"
+        ) %in% names(out)
+      )
+    )
+
+    # Derived column check
+    expect_equal(out$start_year, c(2025L, 2026L))
+  }
+)
+
+# Does the function throw an error when a column is missing?
+
+test_that(
+  "read_flows_2026_publication_data throws an error when required columns are missing",
+  {
+    bad_df <- tibble(
+      phase = "Secondary",
+      subject = "Physics",
+      value = 1.5,
+      academic_year = "2025/26",
+      unit = "FTE",
+      publication_year = 2026
+      # missing type and historic_or_trajectory
+    )
+
+    tmp <- write_test_parquet(bad_df)
+
+    expect_missing_cols_error(
+      read_flows_2026_publication_data(tmp)
+    )
+  }
+)
