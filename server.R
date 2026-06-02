64c6235e7aeca55a48423ec11423ab981d31e131
amsparks <37578485+amsparks@users.noreply.github.com>
2026-06-02 10:43:19 +0100

    Resolve duplicate support_and_feedback ID issue

--- server.R
+++ server.R
@@ -1280,11 +1280,11 @@ server <- function(input, output, session) {
 
 
   # footer links -----------------------
-  shiny::observeEvent(input$accessibility_statement, {
+  shiny::observeEvent(input$accessibility_footer_link, {
     shiny::updateTabsetPanel(session, "navlistPanel", selected = "a11y_panel")
   })
 
-  shiny::observeEvent(input$use_of_cookies, {
+  shiny::observeEvent(input$cookies_footer_link, {
     shiny::updateTabsetPanel(
       session,
       "navlistPanel",
@@ -1292,7 +1292,7 @@ server <- function(input, output, session) {
     )
   })
 
-  shiny::observeEvent(input$support_and_feedback, {
+  shiny::observeEvent(input$support_footer_link, {
     shiny::updateTabsetPanel(
       session,
       "navlistPanel",
@@ -1300,7 +1300,7 @@ server <- function(input, output, session) {
     )
   })
 
-  shiny::observeEvent(input$privacy_notice, {
+  shiny::observeEvent(input$privacy_footer_link, {
     showModal(modalDialog(
       external_link(
         "https://www.gov.uk/government/organisations/department-for-education/about/personal-information-charter", # nolint
@@ -1327,7 +1327,7 @@ server <- function(input, output, session) {
     )
   })
 
-  shiny::observeEvent(input$external_link, {
+  shiny::observeEvent(input$external_footer_link, {
     showModal(modalDialog(
       external_link(
         "https://shiny.posit.co/",
